5302b85e34f151ab5f73f0c95ea4b01c568f333b
mrKru <mr.kru.v@gmail.com>
2016-02-17 16:33:35 +0200

    17.02v2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	tinymce.init({ selector:'textarea.editor' });
 	$('.add-coment').on('click', function() {
-	$('.simple-review-form').show();	
+	$('.simple-review-form').show();
 	});
 	$(".review-form").submit(function() {
 		var now = new Date();
@@ -9,23 +9,23 @@ $(document).ready(function() {
 		var review_data = {
 			username: "",
 			contents: "",
-			date: now.getDate() + "." + now.getMonth() + "." + now.getFullYear(),
+			date: now.getDate() + "." + (now.getMonth()+1) + "." + now.getFullYear(),
 			email: "",
 		};
-		review_data.username = $('input[name="name"]').val();
+		review_data.username = $("input[name='name']").val();
+		review_data.content = $("textarea[name='content']").val();
 		review_data.email = $("input[name='email']").val();
-		review_data.content = $('textarea[name="content"]').val();
 
 		var review_clone = $(".review-template").clone();
 		review_clone.find(".review-date").html(review_data.date);
-		review_clone.find('.review-username').html(review_data.username);
+		review_clone.find(".review-username").html(review_data.username);
+		review_clone.find(".review-content").html(review_data.contents);
 		review_clone.find(".review-email").html(review_data.email);
-		review_clone.find(".review-content").html(review_data.content);		
-		review_clone.removeClass("review-clone");
 
-		$(".review-container").append(review_clone);
-		$('.simple-review-form').hide();
+		$(".review-container").append(review_clone).removeClass(".review-template");
 
+
+		$("simple-review-form input, .simple-review-form textarea").val('');
 
 		return false;
 	});
